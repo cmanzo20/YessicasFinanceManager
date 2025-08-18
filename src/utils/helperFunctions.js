65222e7a9285918bded829f4e0160b43161f1b67
Carlos <cmanzo20@stu.jjc.edu>
2025-08-18 13:38:10 -0500

    attempted to fix error with deployment- changed == to ===

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -1,30 +1,30 @@
 export function daysToString(days) {
-    if (days == 7) {
+    if (days === 7) {
         return "weekly";
     }
-    else if (days == 14) {
+    else if (days === 14) {
         return "biweekly";
     }
-    else if (days == 28) {
+    else if (days === 28) {
         return "monthly";
     }
-    else if (days == 365) {
+    else if (days === 365) {
         return "annually";
     }
     return "every " + days + " days";
 };
 
 export function stringToDays(unit) {    //currently not used due to refactoring, leaving in case of later use
-    if (unit == "day(s)") {
+    if (unit === "day(s)") {
         return parseInt(1);
     }
-    else if (unit == "week(s)") {
+    else if (unit === "week(s)") {
         return parseInt(7);
     }
-    else if (unit == "month(s)") {
+    else if (unit === "month(s)") {
         return parseInt(28);
     }
-    else if (unit == "year(s)") {
+    else if (unit === "year(s)") {
         return parseInt(365);
     }
     return null;
